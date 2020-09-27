73b633d5dbdb15cbc6bd919d2114f9fccf82059c
Abas <uia66835@contiwan.com>
2020-09-27 19:59:24 +0200

    Save

--- scripts/retrain_model.bat
+++ scripts/retrain_model.bat
@@ -3,4 +3,4 @@ echo OFF
 pushd ..\Workspace\training_demo
 SET MODEL_NAME=ssd_mobilenet_v2_320x320_coco17_tpu-8
 
-python model_main_tf2.py --model_dir=models/%MODEL_NAME% --pipeline_config_path=models/%MODEL_NAME%/pipeline.config --checkpoint_dir=models/%MODEL_NAME%
+python model_main_tf2.py --model_dir=models/%MODEL_NAME% --pipeline_config_path=models/%MODEL_NAME%/pipeline.config 
