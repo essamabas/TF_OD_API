dcb3293938072f9c479d7adb71c68a47d4317781
EA <qegabbe@gmail.com>
2020-10-11 16:51:23 +0200

    save notebooks

--- scripts/retrain_model.bat
+++ scripts/retrain_model.bat
@@ -1,6 +1,19 @@
 echo OFF
 
 pushd ..\Workspace\training_demo
-SET MODEL_NAME=ssd_mobilenet_v2_320x320_coco17_tpu-8
+SET MODEL_NAME=ssd_resnet50_v1_fpn_640x640_coco17_tpu-8
 
-python model_main_tf2.py --model_dir=models/%MODEL_NAME% --pipeline_config_path=models/%MODEL_NAME%/pipeline.config 
+python model_main_tf2.py --model_dir=models/%MODEL_NAME% --pipeline_config_path=models/%MODEL_NAME%/pipeline.config
+
+SET PIPELINE_CONFIG_PATH=models/%MODEL_NAME%/pipeline.config
+SET MODEL_DIR=models/%MODEL_NAME%
+SET NUM_TRAIN_STEPS=100
+SET SAMPLE_1_OF_N_EVAL_EXAMPLES=10
+
+rem python model_main_tf2.py -- \
+rem  --model_dir=%MODEL_DIR% --num_train_steps=%NUM_TRAIN_STEPS% \
+rem  --sample_1_of_n_eval_examples=%SAMPLE_1_OF_N_EVAL_EXAMPLES% \
+rem --pipeline_config_path=%PIPELINE_CONFIG_PATH% \
+rem  --alsologtostderr
+
+popd
